f7f73848ed7bbaeb6cd3b5a0f2b73a03da9d5423
gurrola-leslie <60592816+gurrola-leslie@users.noreply.github.com>
2022-07-15 14:58:43 -0700

    added hire() and other method

--- java/com/aa/act/interview/org/Organization.java
+++ java/com/aa/act/interview/org/Organization.java
@@ -1,6 +1,6 @@
 package com.aa.act.interview.org;
-
 import java.util.Optional;
+import java.util.*;
 
 public abstract class Organization {
 
@@ -20,10 +20,56 @@ public abstract class Organization {
 	 * @return the newly filled position or empty if no position has that title
 	 */
 	public Optional<Position> hire(Name person, String title) {
-		//your code here
-		return Optional.empty();
-	}
-
+            
+            //creating a potential employee
+            Random rand = new Random();
+            int intRandom = rand.nextInt(10000);
+            //random number generater 
+            Employee potentialEmployee = new Employee(intRandom, person);
+            Optional<Position> posPosition = positionFinder(title);
+            
+            
+            //checking of positon is empty or if no position has that title
+            if(posPosition.isEmpty()){
+                //return original statement
+                return Optional.empty();
+            }
+            //set employee to positon in set
+            //Optional of() return an optional with specified present non-null value
+            posPosition.get().setEmployee(Optional.of(potentialEmployee));
+            
+            return posPosition;
+            
+        }
+        
+        
+        //sees if position if filled or not 
+        //created a linked list to add position of new and look over positions
+        
+        public Optional<Position> positionFinder(String title)
+        {
+            Position head = root; 
+            boolean isOpen = false; 
+            LinkedList<Position> positionAva = new LinkedList(); 
+            
+            if(!positionAva.isEmpty()){
+                
+                positionAva.add(head);
+            }
+            
+            if(head.getTitle().equals(title) && !head.isFilled()){
+                isOpen = true;
+            }
+            
+            if(!isOpen){
+                //original return statement
+                return Optional.empty();
+            
+            }
+            
+            return Optional.of(head);
+        
+        }
 	@Override
 	public String toString() {
 		return printOrganization(root, "");
